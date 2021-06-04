02a4673bd76d40d6deb3617a2de159bea23724c6
bftawfik <bftawfik@gmail.com>
2021-06-04 06:50:10 +0200

    add shuffleArray

--- src/Helpers/helperFunctions.js
+++ src/Helpers/helperFunctions.js
@@ -1,2 +1,4 @@
 export const joinClassesWithSpace = (...classNames) =>
   classNames.filter((className) => className).join(" ");
+
+export const shuffleArray = (array) => array.sort(() => 0.5 - Math.random());
