2f0b1801aadb0f005c98f2c03d4ce74a99feb73f
bftawfik <bftawfik@gmail.com>
2021-06-04 05:17:56 +0200

    add WelcomeBox

---
+++ src/Components/WelcomeBox/WelcomeBox.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+
+import * as styles from "./WelcomeBox.module.scss";
+
+const WelcomeBox = ({ username, history }) => {
+  const gotoMCQ = () => {
+    history.push("/question/1")
+  }
+  return (
+    <div className={styles.WelcomeBox}>
+      <p>Welcome {username}</p>
+      <button onClick={gotoMCQ}>Start MCQ exam</button>
+    </div>
+  );
+};
+export default withRouter(WelcomeBox);
