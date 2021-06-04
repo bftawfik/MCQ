d7fd0865c9c9f105b251c528de6dcc3ba342e261
bftawfik <bftawfik@gmail.com>
2021-06-04 09:49:47 +0200

    change questions

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -12,32 +12,35 @@ const initState = {
   questions: [
     {
       id: 0,
-      question: "q0",
-      answers: ["a", "b", "c", "d"],
+      question:
+        " If a red house is made from red bricks, a blue house is made from blue bricks, a pink house is made from pink bricks, and a black house is made from black bricks. What is a greenhouse made from?",
+      answers: ["Wood", "Moss covered stones", "Glass", "Green Bricks"],
       correctAnswerNdx: 1,
     },
     {
       id: 1,
-      question: "q1",
-      answers: ["a", "b", "c", "d"],
+      question:
+        "Some months have 31 days, and some months have 30 days. But how many have 28 days",
+      answers: ["1", "2", "4", "8"],
       correctAnswerNdx: 2,
     },
     {
       id: 2,
-      question: "q2",
-      answers: ["a", "b", "c", "d"],
+      question:
+        "John digs a hole that is 2 yards wide, 3 yards long, and 1 yard deep. How many cubic feet of dirt are in it?",
+      answers: ["0", "2", "3", "5"],
       correctAnswerNdx: 0,
     },
     {
       id: 3,
-      question: "q3",
-      answers: ["a", "b", "c", "d"],
+      question: "here did the real Boston Tea Party take place?",
+      answers: ["New york", "Boston", "Washington", "Philadelphia"],
       correctAnswerNdx: 0,
     },
     {
       id: 4,
-      question: "q4",
-      answers: ["a", "b", "c", "d"],
+      question: "What is the capital of Florida?",
+      answers: ["Miami", "Tampa Bay", "Tallahassee", "Orlando"],
       correctAnswerNdx: 0,
     },
   ],
