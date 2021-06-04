4ddb08be5af91c0abab96b58bcf58d83cf77ebe3
bftawfik <bftawfik@gmail.com>
2021-06-04 05:18:41 +0200

    add mock file

---
+++ src/Mock/index.js
@@ -0,0 +1,20 @@
+export default {
+  orgName: "MCQ",
+  questionsBank: [
+    {
+      question: "q",
+      answers: ["a", "b", "c", "d"],
+      correctAnswerNdx: 1,
+    },
+    {
+      question: "q",
+      answers: ["a", "b", "c", "d"],
+      correctAnswerNdx: 2,
+    },
+    {
+      question: "q",
+      answers: ["a", "b", "c", "d"],
+      correctAnswerNdx: 0,
+    },
+  ],
+};
